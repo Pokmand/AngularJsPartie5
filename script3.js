0430bf9a445636e3c2756fea65e9a18b995ce686
Pokmand <manudesj@msn.com>
2019-05-06 16:36:46 +0200

    list topic ok

--- script3.js
+++ script3.js
@@ -1,11 +1,15 @@
 var formApp = angular.module('formApp', ['ngMessages']);
 
-formApp.controller('formCtrl', ['$scope',function($scope){
-$scope.showfunction = function() {
-  $scope.userarray = [{
-    Name : $scope.Name,
-    email : $scope.email,
-    topic : $scope.topic,
-    textarea : $scope.text,
-  }];
-}}]);
+formApp.controller('formCtrl', ['$scope', function($scope) {
+  $scope.userarray = [];
+  $scope.showfunction = function() {
+
+    // ADD A NEW ELEMENT.
+    $scope.userarray.push({
+      Name: $scope.Name,
+      email: $scope.email,
+      topic: $scope.topic,
+      textarea: $scope.textarea
+    });
+  }
+}]);
